21d21c1e31a6464860c2a25bb7c6464885063952
Ayub <ayub.hassan97@gmail.com>
2020-07-28 01:51:54 -0400

    header work

--- index.js
+++ index.js
@@ -333,8 +333,48 @@ app.use(storyRoutes);
 
 app.use(sprintRoutes);
 
-app.get("/test", function (req, res) {
-  res.json("wtf");
+/* Get route for fetching the team of a user. */
+
+app.get("/user/team/:id", function (req, res) {
+  User.findOne({ userID: req.params.id }, function (err, user) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      if (user) {
+        helper.populateTeam(req, res, user.team);
+      } else {
+        res.json({ team: null });
+      }
+    }
+  });
+});
+
+/* Patch route to update user information upon their login, this
+   ensures our application is consistent with the user's google information */
+
+app.patch("/user", function (req, res) {
+  User.findOne({ userID: req.body.userID }, function (err, user) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      user.profilePic = req.body.userpicture;
+      user.name = req.body.username;
+      user.save();
+      helper.populateUser(req, res, user._id);
+    }
+  });
+});
+
+/* Get route to fetch user information upon their login */
+
+app.get("/user/:id", function (req, res) {
+  User.findOne({ userID: req.params.id }, function (err, user) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      helper.populateUser(req, res, user._id);
+    }
+  });
 });
 /*
 
